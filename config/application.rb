a8a1a64ee6bfa03ae4a5b811385a8603a914aa56
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-02-26 17:25:14 +0900

    Add user_name to users

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MyBlog
-  class Application < Rails::
+module MiniApp
+  class Application < Rails::Application
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
