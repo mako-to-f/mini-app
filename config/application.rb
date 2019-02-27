2a89189f9bf33b54dbbee5cce0c024f6b1348b2c
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-02-27 17:26:30 +0900

    Add error_message of sign in

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module MiniApp
       g.helper false
       g.test_framework false
     end
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      %Q(#{html_tag}).html_safe
+    end
     config.i18n.default_locale = :ja
   end
 end
